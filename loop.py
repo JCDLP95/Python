edf31fb0330b52c79e9abe2497f485a6d31933c2
Juan Carlos de la Peña <79996751+JCDLP95@users.noreply.github.com>
2025-03-27 08:43:35 -0600

    Add files via upload

---
+++ loop.py
@@ -0,0 +1,17 @@
+while True:
+  print("Never going to ______ you up.")
+  gi = input("Hint?Starts with the leter g:")
+  if gi == "give":
+   break
+while True:
+  print("Never going to ______ you down.")
+  le = input("Hint?Starts with the leter l:")
+  if le == "let":
+   break
+while True:
+  print("Never going to ______ around.")
+  ru = input("Hint?Starts with the leter r:")
+  if ru == "run":
+   break
+    
+print("And desert you.")
