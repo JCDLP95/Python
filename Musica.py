26cb9ebf6f329d8c26dc1281d55329ba1ef7c676
Juan Carlos de la Peña <79996751+JCDLP95@users.noreply.github.com>
2025-04-17 08:56:24 -0600

    Update Musica.py

--- Musica.py
+++ Musica.py
@@ -5,11 +5,16 @@ print("Welcome to iTunes")
 time.sleep(1)
 os.system("clear")
 
-username = input("Choose 0 to play a song or 1 to exit: ")
+while True:
+    username = input("Choose 0 to play a song or 1 to exit: ")
 
-if username == "0":
-    print("AC/DC, TNT")
-elif username == "1":
-    print("Off")
-else:
-    print("Wrong button")
+    if username == "0":
+        print("AC/DC, TNT")
+        break
+    elif username == "1":
+        print("Off")
+        break
+    else:
+        print("Wrong button. Try again.")
+        time.sleep(1)
+        os.system("clear")
